45609928c336ac29f177427c04b495b78e89f4b5
Colour1205 <Ccccchuck1@gmail.com>
2025-06-03 23:55:19 -0400

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -659,8 +659,3 @@ setThemeBySystem();
 
 // Listen for changes in system theme
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setThemeBySystem);
-
-// Remove no-anim class after page load to enable animations
-window.addEventListener('load', () => {
-    document.body.classList.remove('no-anim');
-});
